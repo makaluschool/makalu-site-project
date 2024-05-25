25c829a27152e67498751781d32edaa2e5aacc3d
poudelkeeshab <keeshabpoudel322@gmail.com>
2024-05-25 15:18:38 +0545

    navbar created

--- makalu-frontend/src/app/page.tsx
+++ makalu-frontend/src/app/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>welcome to makalu school</h1>
-      <Button className="bg-green-700 hover:bg-green-600">click me</Button>
+
     </div>
   )
 }
