af65fb661278a3d60c206a469a6031e07801a924
sohan01fw <sohancwactfw@gmail.com>
2024-08-19 11:03:37 +0545

    fix displaying blog details data

--- makalu-frontend/src/app/blog/[slug]/page.tsx
+++ makalu-frontend/src/app/blog/[slug]/page.tsx
@@ -4,27 +4,27 @@ import React from "react";
 async function page({ params }: { params: { slug: string } }) {
  
   const blogTitle = decodeURIComponent(params.slug)
+  const blogDataDetails = await axios.get(`http://localhost:1337/api/community-blogs?filters[blog_title][$eq]=${blogTitle}&populate=*`)
+  const data = (blogDataDetails.data.data)
   return (
 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div className="max-w-3xl mx-auto">
-        <div className="py-8">
-            <h1 className="text-3xl font-bold mb-2">Blog post title</h1>
-            <p className="text-gray-500 text-sm">Published on <time >April 5, 2022</time></p>
-        </div>
+  {data.map((d:any, index:number) => {
+    const publishedAt = new Date(d.attributes.publishedAt)
+  return(
+      <div className="max-w-3xl mx-auto mt-40">
+      <div className="py-8">
+          <h1 className="text-3xl font-bold mb-2">{d.attributes.blog_title}</h1>
+          <p className="text-gray-500 text-sm">Published on <time >{publishedAt.toLocaleDateString()}</time></p>
+      </div>
 
-        <img src="https://images.unsplash.com/photo-1493723843671-1d655e66ac1c" alt="Featured image" className="w-full h-auto mb-8"/>
+      <img src={`http://localhost:1337${d.attributes.blog_cover_image.data.attributes.url}`} alt="Featured image" className="w-full h-[28rem] mb-8 object-cover"/>
 
-        <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus varius fringilla augue, vel vestibulum
-                nisl mattis vel. Praesent porttitor pharetra purus eu tincidunt.</p>
-            <p>Nullam vitae sapien non est suscipit blandit quis sit amet ipsum. Aliquam euismod accumsan nunc, in
-                convallis felis luctus in. Sed rhoncus metus a elit rutrum aliquam.</p>
-            <p>Integer ullamcorper leo nulla, nec commodo metus vehicula eget. Duis vel vestibulum tellus, eget mattis
-                quam. Nullam euismod libero sed nibh tristique, vel eleifend risus sagittis. In hac habitasse platea
-                dictumst. Sed dapibus magna at arcu euismod, a pulvinar turpis tristique. Suspendisse imperdiet velit
-                nec lectus rutrum varius.</p>
-        </div>
-    </div>
+      <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto">
+          {d.attributes.blog_content}
+      </div>
+  </div>
+  )})}
+    
 </div>
 
   );
