735f88f85f723ccac746a91d03624de84eb335a2
sohan01fw <sohancwactfw@gmail.com>
2024-08-20 18:05:07 +0545

    change footer details

--- makalu-frontend/src/components/Footer.tsx
+++ makalu-frontend/src/components/Footer.tsx
@@ -27,21 +27,21 @@ const Footer = () => {
         <p className="text-base font-bold tracking-wide text-gray-900">Contacts</p>
         <div className="flex">
           <p className="mr-1 text-gray-800">Phone:</p>
-          <a href="tel:850-123-5021" aria-label="Our phone" title="Our phone" className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"> (+977)9852048899
+          <p  aria-label="Our phone" title="Our phone" className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"> (+977)9852048899
 
-</a>
+</p>
         </div>
         <div className="flex">
           <p className="mr-1 text-gray-800">Email:</p>
-          <a href="mebs2057@gmail.com" aria-label="Our email" title="Our email" className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">mebs2057@gmail.com
+          <p aria-label="Our email" title="Our email" className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">mebs2057@gmail.com
 
-</a>
+</p>
         </div>
         <div className="flex">
           <p className="mr-1 text-gray-800">Address:</p>
-          <a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer" aria-label="Our address" title="Our address" className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">
+          <p rel="noopener noreferrer" aria-label="Our address" title="Our address" className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">
           Itahari-05, Sunsari
-          </a>
+          </p>
         </div>
       </div>
       <div>
@@ -62,7 +62,7 @@ const Footer = () => {
               ></path>
             </svg>
           </a>
-          <a href="/" className="text-gray-500 transition-colors duration-300 hover:text-blue-600">
+          <a href="https://www.facebook.com/profile.php?id=100040895302557" target='_blank' className="text-gray-500 transition-colors duration-300 hover:text-blue-600">
             <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
               <path
                 d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
@@ -81,14 +81,10 @@ const Footer = () => {
       </p>
       <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
         <li>
-          <a href="/" className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">F.A.Q</a>
+          <p className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">Made by: sohancwactfw@gmail.com</p>
         </li>
-        <li>
-          <a href="/" className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">Privacy Policy</a>
-        </li>
-        <li>
-          <a href="/" className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">Terms &amp; Conditions</a>
-        </li>
+       
+        
       </ul>
     </div>
   </div></div>
