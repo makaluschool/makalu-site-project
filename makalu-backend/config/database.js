8c102ea15a00f1d643ae0e77d9999739599fdab8
sohan01fw <sohancwactfw@gmail.com>
2024-09-06 13:03:00 +0545

    configure strapi db with supabase db

--- makalu-backend/config/database.js
+++ makalu-backend/config/database.js
@@ -1,30 +1,20 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres'); // Set PostgreSQL as the client
+  const client = env('DATABASE_CLIENT'); // Set PostgreSQL as the client
 
   const connections = {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'), // Change to your actual PostgreSQL host
-        port: env.int('DATABASE_PORT', 5432),    // PostgreSQL default port
-        database: env('DATABASE_NAME', 'strapi'), // Your database name
-        user: env('DATABASE_USERNAME', 'strapi'), // Your PostgreSQL username
-        password: env('DATABASE_PASSWORD', 'strapi'), // Your PostgreSQL password
         ssl: env.bool('DATABASE_SSL', false) && {  // SSL configuration
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
         },
         schema: env('DATABASE_SCHEMA', 'public'), // Default schema
       },
       pool: {
-        min: env.int('DATABASE_POOL_MIN', 2), 
-        max: env.int('DATABASE_POOL_MAX', 10), // Connection pool settings
+        min: env.int('DATABASE_POOL_MIN', 1), 
+        max: env.int('DATABASE_POOL_MAX', 100), // Connection pool settings
       },
     },
   };
--- makalu-backend/config/plugins.js
+++ makalu-backend/config/plugins.js
@@ -1,20 +1,37 @@
 module.exports = ({ env }) => ({
-    email: {
-      config: {
-        provider: 'nodemailer',
-        providerOptions: {
-          host: 'smtp.gmail.com',
-          port: 465,
-          auth: {
-            user: process.env.email, // Your Gmail
-            pass: process.env.psw,    // Gmail App Password
-          },
-        },
-        settings: {
-          defaultFrom: process.env.email,
-          defaultReplyTo: process.env.email,
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: "smtp.gmail.com",
+        port: 465,
+        auth: {
+          user: process.env.email, // Your Gmail
+          pass: process.env.psw, // Gmail App Password
         },
       },
+      settings: {
+        defaultFrom: process.env.email,
+        defaultReplyTo: process.env.email,
+      },
     },
-  });
-  
+  },
+
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-supabase",
+      providerOptions: {
+        apiUrl: env("SUPABASE_API_URL"),
+        apiKey: env("SUPABASE_API_KEY"),
+        bucket: env("SUPABASE_BUCKET"),
+        directory: env("SUPABASE_DIRECTORY"),
+        options: {},
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
